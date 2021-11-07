c63dde31e61046501cc9b321f4c03148f09a1d77
BogdanBunchuk <93079088+BogdanBunchuk@users.noreply.github.com>
2021-11-07 15:29:21 +0200

    little change

--- memory_layout.py
+++ memory_layout.py
@@ -1,7 +1,7 @@
 from typing import Counter
 from PyQt5.QtWidgets import QPushButton, QRadioButton, QSpinBox, QLabel, QGroupBox, QButtonGroup, QVBoxLayout, QHBoxLayout
 from PyQt5.QtCore import Qt
-
+import random
 menu_button = QPushButton('Меню')
 sleep = QPushButton('Отдохнуть')
 box_minutes = QSpinBox()
@@ -22,10 +22,18 @@ RadButton2 = QRadioButton('')
 RadButton3 = QRadioButton('')
 RadButton4 = QRadioButton('')
 
-RadioGroup.addButton(RadButton1)
-RadioGroup.addButton(RadButton2)
-RadioGroup.addButton(RadButton3)
-RadioGroup.addButton(RadButton4)
+RButton_group = [RadButton1, RadButton2, RadButton3, RadButton4]
+random.shuffle(RButton_group)
+
+RadioGroup.addButton(RButton_group[0])
+RadioGroup.addButton(RButton_group[1])
+RadioGroup.addButton(RButton_group[2])
+RadioGroup.addButton(RButton_group[3])
+
+answer1 = RButton_group[0]
+answer2 = RButton_group[1]
+answer3 = RButton_group[2]
+answer4 = RButton_group[3]
 
 submit = QPushButton('Ответить')
 
@@ -33,23 +41,16 @@ layout_ans1 = QHBoxLayout()
 layout_ans2 = QVBoxLayout()
 layout_ans3 = QVBoxLayout()
 
-layout_ans2.addWidget(RadButton1)
-layout_ans2.addWidget(RadButton2)
-layout_ans3.addWidget(RadButton3)
-layout_ans3.addWidget(RadButton4)
+layout_ans2.addWidget(answer1)
+layout_ans2.addWidget(answer2)
+layout_ans3.addWidget(answer3)
+layout_ans3.addWidget(answer4)
 
 layout_ans1.addLayout(layout_ans2)
 layout_ans1.addLayout(layout_ans3)
 
 text_correct = 'Верно'
 text_wrong = 'Неверно'
-
-
-answer4 = RadButton4
-answer1 = RadButton2
-answer2 = RadButton3
-answer3 = RadButton1
-
 
 AnsGroupBox = QGroupBox("Результат теста")
 lb_Result = QLabel('') 
@@ -60,7 +61,6 @@ layout_res.addWidget(result_count, alignment=(Qt.AlignLeft | Qt.AlignTop))
 layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
 AnsGroupBox.setLayout(layout_res)
 AnsGroupBox.hide()
-
 
 layout_test = QHBoxLayout()
 
@@ -80,6 +80,25 @@ RadioGroupBox.setLayout(layout_ans1)
 layout_line3.addWidget(RadioGroupBox, stretch=2)
 layout_line3.addWidget(AnsGroupBox, stretch=2)
 layout_line4.addWidget(submit, stretch=2)
+
+class Question():
+    def __init__(self, question, answer1, answer2, answer3, answer4):
+        self.question = question
+        self.answer1 = answer1
+        self.answer2 = answer2
+        self.answer3 = answer3
+        self.answer4 = answer4
+    def set_question(self):
+        question.setText(self.question)
+        answer1.setText(self.answer1)
+        answer2.setText(self.answer2)
+        answer3.setText(self.answer3)
+        answer4.setText(self.answer4)
+    def got_right(self):
+        print('Это правиьный ответ!')
+    def got_wrong(self):
+        print('Ответ неверный')
+
 
 def show_result():
     RadioGroupBox.hide()
@@ -137,34 +156,22 @@ def show_question():
     RadioGroup.setExclusive(True)
 
     if counter.text() == '0':
-        question.setText('Перевод слова - Яблоко')
+        q1 = Question('Перевод слова - Яблоко', 'Apply', 'Ant', 'Appart', 'Apple')
         lb_Correct.setText('Apple')
-        answer1.setText('Apply')
-        answer2.setText('Ant')
-        answer3.setText('Appart')
-        answer4.setText('Apple')
+        q1.set_question()
         counter.setText('1')
     elif counter.text() == '1':
-        question.setText('Перевод слова - Piggy bank')
         lb_Correct.setText('Свинья копилка')
-        answer1.setText('Банк со свинкой')
-        answer2.setText('Банк свиньи')
-        answer3.setText('Свинья копилка')
-        answer4.setText('Свиной банк')
+        q2 = Question('Перевод слова - Piggy bank', 'Банк со свинкой', 'Банк свиньи', 'Свинья копилка', 'Свиной банк')
+        q2.set_question()
         counter.setText('2')
     elif counter.text() == '2':
-        question.setText('Не являетя правильным переводом - Iron')
         lb_Correct.setText('Мягкий')
-        answer1.setText('Мягкий')
-        answer2.setText('Утюг')
-        answer3.setText('Железный')
-        answer4.setText('Железо')
+        q3 = Question('Не являетя правильным переводом - Iron', 'Мягкий', 'Утюг', 'Железный', 'Железо')
+        q3.set_question()
         counter.setText('3')
     elif counter.text() == '3':
-        question.setText('Слова, которые не были в вопросах')
+        q4 = Question('Слова, которые не были в вопросах', 'Apple', 'Свинья копилка', 'Iron', 'Твёрдость')
         lb_Correct.setText('Твёрдость')
-        answer1.setText('Apple')
-        answer2.setText('Свинья копилка')
-        answer3.setText('Iron')
-        answer4.setText('Твёрдость')
+        q4.set_question()
         counter.setText('4')
